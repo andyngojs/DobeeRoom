46f63dc6d8fc6877c9ed595e065ae12e2ad9e487
andyngojs <dongngo.2000@gmail.com>
2022-03-13 17:16:11 +0700

    #3: change icon and title

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,5 +1,12 @@
+import { useEffect } from 'react';
+
 export default function HomePage() {
+
+    useEffect(() => {
+        document.title = 'Trang Chủ | DobeeRoom - Hỗ trợ sinh viên tìm nhà trọ';
+    }, []);
+
     return (
         <div>hello HomePage!</div>
-    )
-}
+    );
+};
